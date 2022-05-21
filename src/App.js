e66012dd28ccaf8ecaa73d9c205fc24c04e7fbdf
Rman <>
2022-05-21 17:33:26 +0430

    commmit problem fix

--- src/App.js
+++ src/App.js
@@ -5,51 +5,48 @@ import React, { useEffect, useState, useCallback } from "react";
 import loadingUrl from "./img/loading.svg";
 function App() {
 	const [urlData, setUrlData] = useState({});
-	const [errorMessage, setErrorMessage] = useState("");
-	const [tabUrl, setTabUrl] = useState("");
+	const [errorMessage, setErrorMessage] = useState("some error happened  ❌");
 	const [loading, setLoading] = useState(true);
 
-	const getUrl = useCallback(() => {
-		setLoading(true);
-		browser.tabs.query({ currentWindow: true, active: true }).then((tabs) => {
-			const x = new URL(tabs[0].url).hostname;
-			console.log(x);
-			setTabUrl(x);
+	const getUrl = async () => {
+		let urlTab = "";
+		const tabs = await browser.tabs.query({
+			currentWindow: true,
+			active: true,
 		});
-	}, [setUrlData]);
+		console.log(urlTab);
+		const getUrlTab = new URL(await tabs[0].url).hostname;
+		
+		return getUrlTab
+	};
 
 	const fetchApi = useCallback(async () => {
+
 		try {
 			setLoading(true);
-			if (tabUrl === "") return;
-			const response = await fetch(`http://ip-api.com/json/${tabUrl}`);
+			const response = await fetch(`http://ip-api.com/json/${await getUrl()}`);
+			if (!response.ok)
+				throw new Error(`something went wrong error ${response.status} ❌`);
 			const jsonResponse = await response.json();
-			if (jsonResponse.status === "fail")
-				throw new Error("something went wrong");
 			setUrlData(jsonResponse);
 		} catch (err) {
 			setErrorMessage(err.message);
 		}
 		setLoading(false);
-	}, [tabUrl]);
-	useEffect(() => {
-		getUrl();
-	}, [getUrl]);
+	}, []);
+
 	useEffect(() => {
 		fetchApi();
 	}, [fetchApi]);
-	console.log("red");
 	return (
 		<div className="app">
 			{loading ? (
 				<img src={loadingUrl} />
 			) : (
-				(
-					<>
-						<p> country: {urlData.country}</p>
-						<p> city: {urlData.city}</p>
-					</>
-				) ?? errorMessage
+				<>
+					<p> country: {urlData.country ?? errorMessage}</p>
+					<p> city: {urlData.city ?? errorMessage}</p>
+				</>
 			)}
 		</div>
 	);
